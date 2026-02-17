f565d59e31a234edd057c08f8af9e7859925ce15
eben-vranken <vranken.eben@outlook.com>
2026-02-17 21:21:18 +0100

    Implemented navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,13 +1,17 @@
 import { Component, inject, Input } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoneyBill } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.html',
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
 })
 export class Navbar {
   @Input() authService: any;
+
+  faMoneyBill = faMoneyBill;
 
   router = inject(Router);
 
