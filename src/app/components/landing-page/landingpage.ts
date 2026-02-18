436a72475146ac9cdd464712b9258e1face8631b
eben-vranken <vranken.eben@outlook.com>
2026-02-18 14:29:41 +0100

    initial hero section landing page + inter install

--- src/app/components/landing-page/landingpage.ts
+++ src/app/components/landing-page/landingpage.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'landing-page',
   templateUrl: './landingpage.html',
+  imports: [RouterLink]
 })
-export class LandingPage {}
+export class LandingPage { }
