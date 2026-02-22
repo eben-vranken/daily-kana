a18f0d21ce361aff8680183b31f5955dfe87214b
eben-vranken <vranken.eben@outlook.com>
2026-02-22 17:37:10 +0100

    Updated navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,7 +1,7 @@
 import { Component, inject, Input, signal } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBars, faMoneyBill, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faLanguage, faMoneyBill, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'navbar',
@@ -13,12 +13,12 @@ export class Navbar {
 
   isMenuOpen = signal(false);
 
-  faMoneyBill = faMoneyBill;
+  faLanguage = faLanguage;
   faBars = faBars;
   faXmark = faXmark;
 
   toggleMenu() {
-    this.isMenuOpen.update(val => !val);
+    this.isMenuOpen.update((val) => !val);
 
     const isOpen = this.isMenuOpen();
     document.body.style.overflow = isOpen ? 'hidden' : 'auto';
